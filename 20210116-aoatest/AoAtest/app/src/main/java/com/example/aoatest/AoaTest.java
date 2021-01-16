8ac91937f23ad69c3fc56c39ec5c76a3dcb364fe
Zou Hanya <hanyazou@gmail.com>
2021-01-16 11:14:16 +0900

    Add AoaTest which handles USB accessory

---
+++ 20210116-aoatest/AoAtest/app/src/main/java/com/example/aoatest/AoaTest.java
@@ -0,0 +1,99 @@
+package com.example.aoatest;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+public class AoaTest implements Runnable {
+    private static final String TAG = "AoaTest";
+
+    FileInputStream mInputStream;
+    FileOutputStream mOutputStream;
+    Thread mThread;
+
+    public void start(ParcelFileDescriptor fileDescriptor) {
+        FileDescriptor fd = fileDescriptor.getFileDescriptor();
+        mInputStream = new FileInputStream(fd);
+        mOutputStream = new FileOutputStream(fd);
+        mThread = new Thread(null, this, "AosTest");
+        mThread.start();
+    }
+
+    public void stop() {
+        if (mInputStream != null) {
+            try {
+                mInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mInputStream = null;
+        }
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mOutputStream = null;
+        }
+
+        if (mThread != null) {
+            Log.d(TAG, "interrpt thread to stop");
+            mThread.interrupt();
+            Log.d(TAG, "wait...");
+            try {
+                mThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "wait...done");
+            mThread = null;
+        }
+    }
+
+    public void run() {
+        int ret = 0;
+        byte[] buffer = new byte[4096];
+
+        try {
+            Thread.sleep(1000);
+            String str = "Hello";
+            send(str.getBytes());
+        } catch (Exception e) {
+        }
+        while (mInputStream != null) {
+            try {
+                ret = mInputStream.read(buffer);
+            } catch (IOException e) {
+                Log.e(TAG, "read failed", e);
+                break;
+            }
+            Log.d(TAG, ret + " bytes read from USB accessory.");
+            Log.d(TAG, Utils.bytesToHex(buffer));
+        }
+        Log.d(TAG, "thread terminated.");
+    }
+
+    public void send(byte[] buffer) {
+        for (int i = 0; i < 10; i++) {
+            if (mOutputStream != null) {
+                try {
+                    Log.d(TAG, "send data to the stream.");
+                    mOutputStream.write(buffer);
+                    break;
+                } catch (IOException e) {
+                    Log.e(TAG, "write failed", e);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
--- 20210116-aoatest/AoAtest/app/src/main/java/com/example/aoatest/MainActivity.java
+++ 20210116-aoatest/AoAtest/app/src/main/java/com/example/aoatest/MainActivity.java
@@ -1,14 +1,110 @@
 package com.example.aoatest;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private UsbManager mUsbManager;
+    UsbAccessory mAccessory;
+    ParcelFileDescriptor mFileDescriptor;
+    final AoaTest mAoaTest = new AoaTest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate");
+        mUsbManager = (UsbManager) getApplicationContext().getSystemService(Context.USB_SERVICE);
+        IntentFilter filter = new IntentFilter(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+
+        if (mAccessory != null) {
+            return;
+        }
+
+        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+        if (accessory != null) {
+            if (mUsbManager.hasPermission(accessory)) {
+                openAccessory(accessory);
+            }
+        } else {
+            Log.d(TAG, "mAccessory is null");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        closeAccessory();
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
+    }
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG,"mUsbReceiver.onReceive");
+            String action = intent.getAction();
+            if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                Log.d(TAG,"mUsbReceiver.onReceive DETACHED");
+                UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                if (accessory != null && accessory.equals(mAccessory)) {
+                    closeAccessory();
+                }
+            }
+        }
+    };
+
+    private void openAccessory(UsbAccessory accessory) {
+        Log.d(TAG,"openAccessory");
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            mAoaTest.start(mFileDescriptor);
+        } else {
+            Log.e(TAG, "accessory open failed");
+        }
+    }
+
+    private void closeAccessory() {
+        Log.d(TAG,"closeAccessory");
+        mAoaTest.stop();
+        try {
+            if (mFileDescriptor != null) {
+                mFileDescriptor.close();
+            }
+        } catch (IOException e) {
+        } finally {
+            mFileDescriptor = null;
+            mAccessory = null;
+        }
     }
 }
