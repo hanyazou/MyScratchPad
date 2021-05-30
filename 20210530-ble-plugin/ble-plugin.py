c632a8e1a506c0d8425421b80b46819cbbaceb56
Zou Hanya <hanyazou@gmail.com>
2021-05-30 23:03:55 +0900

    20210530-ble-plugin: updated

--- 20210530-ble-plugin/ble-plugin.py
+++ 20210530-ble-plugin/ble-plugin.py
@@ -18,7 +18,16 @@ class BleReader:
 
     def detection_handler(self, device, advertisement_data):
         if str(device) not in self.__devices.keys():
-            self.__devices[str(device)] = device
+            # print('### new device {} {}'.format(device, advertisement_data))
+            self.__devices[str(device)] = { 'device': device, 'instances': [] }
+            for service in advertisement_data.service_uuids:
+                # print('search {} in {}'.format(service, list(self.__services.keys())))
+                service = service.lower()
+                if not service in self.__services:
+                    continue
+                plugin = self.__services[service][0]['class']
+                print('    {}: serivice={}, plugin={}, connect={}'.format(device, service, plugin.name, plugin.services[service]['connect']))
+
             data = advertisement_data.manufacturer_data
             keys = list(data.keys())
             if 1 <= len(keys) and keys[0] in companyids:
@@ -27,7 +36,6 @@ class BleReader:
                 manufacturer = 'unknown'
 
             print('{}: {} {} {}'.format(device.address, manufacturer, device.name, device.rssi))
-            # print('    {}'.format(advertisement_data))
 
         for service in advertisement_data.service_data:
             # print('service={} {}'.format(service, self.__services.keys()))
@@ -57,17 +65,19 @@ class BleReader:
         loop.run_until_complete(self.asyncRun(loop))
 
     def install_plugin(self, plugin):
-        for service in plugin.services:
-            print('install plugin={}, service={}'.format(plugin.__name__, service))
+        for service in plugin.services.keys():
+            print('install plugin={}, service={}'.format(plugin.__name__, plugin.services[service]))
             if not service in self.__services:
                 self.__services[service] = []
             self.__services[service].append({ 'class': plugin, })
 
 class BlePlugin:
     name = 'SwitchBot'
-    services = [
-        '00000d00-0000-1000-8000-00805f9b34fb'
-    ]
+    services = {
+        '00000d00-0000-1000-8000-00805f9b34fb': {
+            'connect': False,
+        },
+    }
 
     @staticmethod
     def update(device, address, service, characteristic, data):
@@ -80,8 +90,20 @@ class BlePlugin:
         humidity = data[5] & 0x7f
         print('    {}% {}C {}% {}'.format(remaining_battery, temperature, humidity, device.rssi))
 
+class BlePlugin2:
+    name = 'FS9721-LP3'
+    services = {
+        '0000ffb0-0000-1000-8000-00805f9b34fb': {
+            'connect': True,
+        },
+    }
+
+    def update(self, device, address, service, characteristic, data):
+        print('    PLUGIN: {}'.format(self.name))
+
 __plugins__ = [
-    BlePlugin
+    BlePlugin,
+    BlePlugin2,
 ]
 
 br = BleReader()
